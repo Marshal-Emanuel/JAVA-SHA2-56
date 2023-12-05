36fba27c25c1799c4af5e53829a5592498c89438
Marshal-Emanuel <marshcodes@gmail.com>
2023-12-05 15:52:35 +0300

    readme update

---
+++ README.md.txt
@@ -0,0 +1,21 @@
+# SHA256
+
+This Java program implements the SHA-256 hashing algorithm.
+SHA-256 is a cryptographic hash function that produces a 256-bit (32-byte) hash value from any input. 
+It is widely used for security purposes, such as password hashing, digital signatures, and encryption.
+
+## Installation
+
+To run this program, you need to have Java installed on your system. You can download Java from [here](https://www.oracle.com/java/technologies/javase-downloads.html). Additionally, you need to have a Java IDE, such as [Eclipse](https://www.eclipse.org/downloads/), [NetBeans](https://netbeans.apache.org/download/index.html), or [IntelliJ IDEA](https://www.jetbrains.com/idea/download/).
+
+## Usage
+
+Follow these steps to use the program:
+
+1. Open the Java IDE of your choice and create a new Java project.
+2. Copy and paste the code from this file into a new Java class named `SHA256`.
+3. Save the file and run the program.
+4. The program will display a welcome message and prompt you to enter the input to be hashed.
+5. Enter the input and press enter. The program will process the input and display the hashed output in hexadecimal format.
+6. The program will also display a message in ASCII art.
+
---
+++ SHA256.java
@@ -0,0 +1,87 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class SHA256 {
+
+    public void hash(String pin) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA256");
+            md.update(pin.getBytes());
+            byte[] output = md.digest();
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : output) {
+                sb.append(String.format("%02x", b));
+
+            }
+            System.out.println("||              Process Completed Succesfuly :)           ||");
+            System.out.println("||                                                        ||");
+            System.out.println("||              Hashed Output: \\/                         ||");
+            System.out.println("||                                                        ||");
+
+            System.out.println(sb.toString());
+            System.out.println("||                                                        ||");
+            System.out.println("||                                                        ||");
+        }
+
+        catch (Exception e) {
+            System.out.println("Error ");
+
+        }
+
+    }
+
+    public void input() {
+
+        System.out.println("===========================================================");
+        System.out.println("||                   {'_'}                                ||");
+        System.out.println("||    [[  Welcome to the \"SHA\" Hashing Algorithm  ]]      ||");
+        System.out.println("||                                                        ||");
+        System.out.println("||        Enter the input to be hashed below              ||");
+        System.out.println("||                                                        ||");
+        System.out.println("||        Enter the input to be hashed below              ||");
+
+        Scanner scan = new Scanner(System.in);
+        String password = scan.nextLine();
+        System.out.println("||                Input Recieved                          ||");
+        System.out.println("||          Wait as we do the processing                  ||");
+        System.out.println("||                                                        ||");
+
+        for (int i = 0; i < 20; i++) {
+            try {
+                Thread.sleep(300);
+                System.out.print("| ");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(300);
+                System.out.print("_");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        System.out.println("");
+        System.out.println("||                                                        ||");
+
+        hash(password);
+    }
+
+    public static void main(String[] args) {
+
+        SHA256 sh = new SHA256();
+        sh.input();
+
+        System.out.println("     ██████╗░██╗░░░██╗███████╗\r\n" + //
+                "     ██╔══██╗╚██╗░██╔╝██╔════╝\r\n" + //
+                "     ██████╦╝░╚████╔╝░█████╗░░\r\n" + //
+                "     ██╔══██╗░░╚██╔╝░░██╔══╝░░\r\n" + //
+                "     ██████╦╝░░░██║░░░███████╗\r\n" + //
+                "     ╚═════╝░░░░╚═╝░░░╚══════╝");
+    }
+
+}
